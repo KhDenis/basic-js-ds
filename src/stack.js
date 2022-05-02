7289cf9cac74f03e158b8b4660f1a20c687d4794
Denis <dennimail@gmail.com>
2022-05-03 01:33:59 +0300

    feat: stack is done

--- src/stack.js
+++ src/stack.js
@@ -7,27 +7,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * const stack = new Stack();
  *
  * stack.push(1); // adds the element to the stack
- * stack.peek(); // returns the peek, but doesn't delete it, returns 1
+ * stack.peek(); //   but doesn't delete it, returns 1
  * stack.pop(); // returns the top element from stack and deletes it, returns 1
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+ 
 
+class Stack {
+  constructor(){
+    this.stack = []
+  }
+  push(element) {
+    this.stack.push(element)
+  }
+  
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.pop()
+    
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack[this.stack.length-1]
   }
 }
+// const stack = new Stack();
+// console.log(stack)
+// stack.push(2)
+// console.log(stack)
+//  console.log(stack.peek())
+ 
+//  console.log(stack.pop())
+//  console.log(stack)
 
 module.exports = {
   Stack
